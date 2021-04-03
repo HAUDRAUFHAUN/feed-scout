4334a0dd8ee68632c91df03c6d858ac79007bd88
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-04-03 15:26:25 +0200

    create placeholder for popular feeds section

--- lib/pages/AddFeed.dart
+++ lib/pages/AddFeed.dart
@@ -51,50 +51,62 @@ class _AddFeedState extends State<AddFeed> {
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Form(
-          child: Column(
-            key: _formKey,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter a name for your new feed',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a name for your feed';
-                  }
-                  return value;
-                },
+        child: Column(
+          children: <Widget>[
+            Form(
+              child: Column(
+                key: _formKey,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter a name for your new feed',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a name for your feed';
+                      }
+                      return value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter the url of your new feed',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the url of your feed';
+                      }
+                      return value;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate will return true if the form is valid, or false if
+                        // the form is invalid.
+                        if (_formKey.currentState.validate()) {
+                          print("validated");
+                          _saveFeed();
+                        }
+                      },
+                      child: Text('Safe feed'),
+                    ),
+                  ),
+                ],
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter the url of your new feed',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter the url of your feed';
-                  }
-                  return value;
-                },
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Validate will return true if the form is valid, or false if
-                    // the form is invalid.
-                    if (_formKey.currentState.validate()) {
-                      print("validated");
-                      _saveFeed();
-                    }
-                  },
-                  child: Text('Safe feed'),
-                ),
-              ),
-            ],
-          ),
+            ),
+            Text(
+              "Popular feeds",
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+                "This section is still in development progress. Here you will get some popular feeds to add in the future.")
+            // TODO: Section with popular feeds so that less technical expirienced people can get some point to start.
+          ],
         ),
       ),
     );
